3d0a2c50816000ab00f891b89038fe84e4f08a9f
Aziz <80921378+aziz-zai@users.noreply.github.com>
2023-11-18 18:48:10 +0100

    website done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import logo from "./fatih.png";
+import logo from "./nargilando_logo.png";
 import time from "./time.svg";
 import warning from "./warning.svg";
 import web from "./web.svg";
@@ -11,8 +11,9 @@ function App() {
       <div className="card-wrapper">
         <div className="info-card div1">
           <div className="card-text">
-            Wir sind umgezogen. Besuchen Sie uns gerne in den Schmiedeäckern 19,
-            70599, Ulm.
+            Freunde, der Online-shop ist umgezogen. Im Laden sind wir aber wie
+            gewohnt für euch da. <br /> Nargilando 0711, Königstraße 20, 70173
+            Stuttgart.
             <img alt="warning-icon" src={warning} className="warning-icon" />
           </div>
         </div>
@@ -21,13 +22,13 @@ function App() {
             Öffnungszeiten:
             <div className="oz-wrapper">
               {[
-                { day: "Mo", time: "8:00-8:30" },
-                { day: "Di", time: "8:00-8:30" },
-                { day: "Mi", time: "8:00-8:30" },
-                { day: "Do", time: "8:00-8:30" },
-                { day: "Fr", time: "8:00-8:30" },
-                { day: "Sa", time: "8:00-8:30" },
-                { day: "So", time: "8:00-8:30" },
+                { day: "Mo", time: "12:00-21:00" },
+                { day: "Di", time: "12:00-21:00" },
+                { day: "Mi", time: "12:00-21:00" },
+                { day: "Do", time: "12:00-21:00" },
+                { day: "Fr", time: "12:00-21:00" },
+                { day: "Sa", time: "12:00-21:00" },
+                { day: "So", time: "Geschlossen" },
               ].map((item, index) => (
                 <div className="öffnungszeiten">
                   <div className="day">{item.day}</div>
@@ -39,12 +40,9 @@ function App() {
           </div>
         </div>
         <div className="info-card div3">
-          <div>Oder besuchen Sie uns online:</div>
-          <a
-            style={{ "text-transform": "none" }}
-            href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-          >
-            www.nargilando.de
+          <div>Oder besucht unseren Partnershop:</div>
+          <a style={{ "text-transform": "none" }} href="https://thesmokery.de">
+            thesmokery.de
           </a>
           <img alt="warning-icon" src={web} className="warning-icon" />
         </div>
